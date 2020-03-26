e97358b023ccdaf278b6359d17de4ad5cc503488
Bettineee <60099532+Bettineee@users.noreply.github.com>
2020-03-26 14:38:35 +0100

    Update main.js

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,6 +1,37 @@
 $(document).ready(function () {
     $('.menu-toggler').on('click', function () {
-        $(this).toggleClass('open');
-        $('.top-nav').toggleClass('open');
+    $(this).toggleClass('open');
+    $('.top-nav').toggleClass('open');
     });
-});
+    
+    $('.top-nav .nav-list').on('click', function () {
+    $('.menu-toggler').removeClass('open');
+    $('.top-nav').removeClass('open');
+    });
+    
+    AOS.init({
+    easing: 'ease',
+    duration: 1800
+    });
+    });
+    
+    //Get the button:
+    mybutton = document.getElementById("myBtn");
+    
+    // When the user scrolls down 40px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    mybutton.style.display = "block";
+    } else {
+    mybutton.style.display = "none";
+    }
+    }
+    
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+    
